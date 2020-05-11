d687da6bf6825584de885b9f2ac578402c60b0b7
Gerd Watmann <watmann@de.ibm.com>
2020-05-11 15:53:13 +0200

    create openshift cluster

--- cluster-in-resource-group/main.tf
+++ cluster-in-resource-group/main.tf
@@ -15,14 +15,16 @@ resource "ibm_resource_group" "bootcamp_rg" {
 #}
 
 # Create a cluster in the created resource group
-#resource "ibm_container_cluster" "cluster" {
-#  name              = var.cluster_name
-#  datacenter        = var.datacenter
-#  default_pool_size = var.default_pool_size
-#  machine_type      = var.machine_type
-#  hardware          = var.hardware
-#  kube_version      = var.kube_version
-#  public_vlan_id    = var.public_vlan_id
-#  private_vlan_id   = var.private_vlan_id
-#  resource_group_id = data.ibm_resource_group.resource_group.id
-#}
+resource "ibm_container_cluster" "cluster" {
+  name              = var.cluster_name
+  datacenter        = var.datacenter
+  default_pool_size = var.default_pool_size
+  machine_type      = var.machine_type
+  hardware          = var.hardware
+  kube_version      = var.kube_version
+  public_vlan_id    = var.public_vlan_id
+  private_vlan_id   = var.private_vlan_id
+  resource_group_id = ibm_resource_group.bootcamp_rg.id
+
+  depends_on = [ibm_resource_group.bootcamp_rg]
+}
